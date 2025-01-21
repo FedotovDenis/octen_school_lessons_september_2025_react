c40123f90d3e931c093e002bd07c1cf73e833ea9
denis <denisfedotov2107@gmail.com>
2025-01-21 19:38:50 +0200

    lesson 1 Tailwind

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -4,7 +4,7 @@ import './MyComponent.css';
 
 const MyComponent: FC<MyComponentPropType> = ({text}) => {
   return (
-      <div className={'target'}>
+      <div className='text-3xl font-bold underline'>
           {text}
         </div>
   );
