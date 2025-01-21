b8126c858b72032dea15c85e2004dfc3521f0e99
denis <denisfedotov2107@gmail.com>
2025-01-21 16:05:51 +0200

    lesson 1 component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
   return (
     <>
-            Hello World
+        <MyComponent text={'hello 1'}/>
+        <MyComponent text={'hello 2'}/>
+        <MyComponent text={'hello 3'}/>
     </>
   )
 }
