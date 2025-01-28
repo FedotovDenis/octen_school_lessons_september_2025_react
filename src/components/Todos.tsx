71a02a77356455dda64f3304780c5d1a5d157e76
denis <denisfedotov2107@gmail.com>
2025-01-28 23:07:29 +0200

    home work 2 task 1

---
+++ src/components/Todos.tsx
@@ -0,0 +1,28 @@
+import {Todo} from "./todo/Todo.tsx";
+import {useState, useEffect} from "react";
+import {ITodo} from '../models/ITodo.ts';
+
+
+export const Todos = () => {
+    const [todos, setTodos] = useState<ITodo[]>([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then((value) => value.json())
+            .then((iTodos: ITodo[]) => {
+                setTodos(iTodos);
+            });
+
+
+    }, []);
+
+    return (
+        <>
+            {
+                todos.map(todo => <Todo key={todo.id} item={todo}/>)
+            }
+        </>
+    );
+};
+
+export default Todos;
